807011828b2b74fdc5ccaee24b96650d6a128abb
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-06-02 16:36:59 +0200

    Added validation for the user registration endpoint.

---
+++ validation/userValidation.js
@@ -0,0 +1,27 @@
+const Joi = require('joi');
+
+module.exports = function validateUser(user) {
+    const schema = Joi.object(
+        {
+            username: Joi.string().min(6).max(255).required().messages({
+                'string.empty': 'Username cannot be empty.',
+                'string.min': 'The username has to have at least 6 characters.',
+                'string.max': 'The username cannot excede 255 characters.',
+                'any.required': 'You must enter a username.'
+            }),
+            email: Joi.string().email().max(255).required().messages({
+                'string.empty': 'Email cannot be empty.',
+                'string.email': 'The email must be a valid email address.',
+                'string.max': 'The email cannot excede 255 characters.',
+                'any.required': 'You must enter an email address.'
+            }),
+            password: Joi.string().min(8).max(255).required().messages({
+                'string.empty': 'Password cannot be empty.',
+                'string.min': 'The password has to have at least 8 characters',
+                'string.max': 'The password cannot excede 255 characters.',
+                'any.required': 'You must enter a password.'
+            })
+        }
+    );
+    return schema.validate(user);
+}
