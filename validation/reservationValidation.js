a2484b17a8ed32f6158c3f5957c8197e235281d0
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-06-02 16:44:54 +0200

    Added validation for the Reservation object.

---
+++ validation/reservationValidation.js
@@ -0,0 +1,28 @@
+const { ObjectId } = require('mongodb');
+const Joi = require('joi');
+
+module.exports = function validateScreening(screening) {
+    const schema = Joi.object(
+        {
+            userId: Joi.string().required().custom((value, helpers) => {
+                if (!ObjectId.isValid(value)) {
+                    return helpers.error('any.invalid');
+                }
+            }).messages({
+                'string.empty': 'UserId cannot be empty.',
+                'any.invalid': 'The userId has to be a valid ObjectId.',
+                'any.required': 'You must enter a userId.'
+            }),
+            screeningId: Joi.string().required().custom((value, helpers) => {
+                if (!ObjectId.isValid(value)) {
+                    return helpers.error('any.invalid');
+                }
+            }).messages({
+                'string.empty': 'ScreeningId cannot be empty.',
+                'any.invalid': 'The screeningId has to be a valid ObjectId.',
+                'any.required': 'You must enter a screeningId.'
+            })
+        }
+    );
+    return schema.validate(screening);
+}
