bcc197fa810106af3d99e8fcc6b95e3244778dc2
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-06-02 16:22:16 +0200

    Added validation for the Hall object.

---
+++ validation/hallValidation.js
@@ -0,0 +1,31 @@
+const { ObjectId } = require('mongodb');
+const Joi = require('joi');
+
+module.exports = function validateHall(hall) {
+    const schema = Joi.object(
+        {
+            name: Joi.string().min(5).max(255).required().messages({
+                'string.empty': 'Name cannot be empty.',
+                'string.min': 'The name has to have at least 5 characters.',
+                'string.max': 'The name cannot excede 255 characters.',
+                'any.required': 'You must enter a name.'
+            }),
+            numberOfSeats: Joi.number().min(10).max(50).required().messages({
+                'number.base': 'The number of seats must be a valid number.',
+                'number.min': 'The hall has to have at least 10 seats.',
+                'number.max': 'The hall cannot excede 50 seats.',
+                'any.required': 'You must enter the number of seats in a hall.'
+            }),
+            cinemaId: Joi.string().required().custom((value, helpers) => {
+                if(!ObjectId.isValid(value)){
+                    return helpers.error('any.invalid');
+                }
+            }).messages({
+                'string.empty': 'CinemaId cannot be empty.',
+                'any.invalid': 'The cinemaId has to be a valid ObjectId.',
+                'any.required': 'You must enter a cinemaId.'
+            })
+        }
+    );
+    return schema.validate(hall);
+}
