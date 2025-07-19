d271666920c885c5618821153d0a55c903ba1c53
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2025-07-19 21:51:43 +0200

    Applied RESTful practices to the auth and admin middleware functions.

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,15 +2,15 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
     const token = req.header('x-auth-token');
-    if (!token) return res.status(401).send("Access denied. No token provided.");
+    if (!token) return res.status(401).send({ message: "Access denied. No token provided." });
     try {
         const decoded_payload = jwt.verify(token, 'jwtPrivateToken');
         req.user = decoded_payload;
         next();
     } catch (ex) {
         if (ex.name === 'TokenExpiredError') {
-            return res.status(401).send('Token expired.');
+            return res.status(401).send({ message: 'Token expired.' });
         }
-        res.status(400).send('Invalid token.');
+        res.status(400).send({ message: 'Invalid token.' });
     }
 }
